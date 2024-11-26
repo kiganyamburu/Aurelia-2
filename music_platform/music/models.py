047974efee41892e67998e7aa45f38e06ba5ab06
kiganyamburu <mburukiganya@gmail.com>
2024-11-26 10:25:13 +0300

    working on the music model.py

--- music_platform/music/models.py
+++ music_platform/music/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Song(models.Model):
+    spotify_id = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+
+class Playlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    songs = models.ManyToManyField(Song)
+
+class Follower(models.Model):
+    user = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
